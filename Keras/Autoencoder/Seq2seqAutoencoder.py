4b7eb4e570fab46dc10afc6b46c49a5b8954b93f
lizhi97 <lizhi97@gmail.com>
2018-12-06 14:00:44 +0800

    add autoencoder

---
+++ Keras/Autoencoder/Seq2seqAutoencoder.py
@@ -0,0 +1,13 @@
+from keras.layers import Input, LSTM, RepeatVector
+from keras.models import Model
+
+input_dim = 32
+inputs = Input(shape=(timesteps, input_dim))
+encoded = LSTM(latent_dim)(inputs)
+
+decoded = RepeatVector(timesteps)(encoded)
+decoded = LSTM(input_dim, return_sequences=True)(decoded)
+
+sequence_autoencoder = Model(inputs, decoded)
+encoder = Model(inputs, encoded)
+
